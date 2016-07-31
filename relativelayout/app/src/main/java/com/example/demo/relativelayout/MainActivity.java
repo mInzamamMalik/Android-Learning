51064cf73bc5b5f81b4ff76d5d65377a9208ca87
Inzamam Malik <malikasinger1@gmail.com>
2016-07-31 16:26:00 +0500

    relative layout lab practice

---
+++ relativelayout/app/src/main/java/com/example/demo/relativelayout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.demo.relativelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
